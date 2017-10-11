e49ceafc81f3593505cd04fe9fde3d0e2d217597
harikmr2795 <harikmr2795@gmail.com>
2017-10-11 10:29:49 +0530

    fixed some bugs in deleteHandler

--- main.py
+++ main.py
@@ -11,25 +11,6 @@ class Show(ndb.Model):
 	capacity = ndb.IntegerProperty(indexed = True)
 	available = ndb.IntegerProperty(indexed = True)
 
-class Add(webapp2.RequestHandler):
-	def post(self):
-		s = Show()
-		s.name = self.request.get('show_name')
-		s.available = s.capacity = int(self.request.get('capacity'))
-		s.put()
-		template = JINJA_ENVIRONMENT.get_template('add.html')
-		self.response.out.write(template.render())
-
-class Delete(webapp2.RequestHandler):
-	def post(self):
-		name = self.request.get('name')
-		show_lst = Show.query()
-		search_query = show_lst.filter(Show.name == name)
-		for item in search_query:
-			item.key.delete()
-		self.redirect("/delete.html",{})
-
-
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 		template = JINJA_ENVIRONMENT.get_template('index.html')
@@ -46,6 +27,9 @@ class BuyHandler(webapp2.RequestHandler):
 		}
 		template = JINJA_ENVIRONMENT.get_template('buy.html')
 		self.response.out.write(template.render(template_vars))
+
+    def post(self):
+		pass
 
 class SoldHandler(webapp2.RequestHandler):
     def get(self):
@@ -68,6 +52,13 @@ class AddHandler(webapp2.RequestHandler):
 		template = JINJA_ENVIRONMENT.get_template('add.html')
 		self.response.out.write(template.render(template_vars))
 
+    def post(self):
+		s = Show()
+		s.name = self.request.get('show_name')
+		s.available = s.capacity = int(self.request.get('capacity'))
+		s.put()
+		self.get()
+
 class DeleteHandler(webapp2.RequestHandler):
     def get(self):
 		show_lst = Show.query()
@@ -80,6 +71,14 @@ class DeleteHandler(webapp2.RequestHandler):
 		template = JINJA_ENVIRONMENT.get_template('delete.html')
 		self.response.out.write(template.render(template_vars))
 
+    def post(self):
+		name = self.request.get('name')
+		show_lst = Show.query()
+		search_query = show_lst.filter(Show.name == name)
+		for item in search_query:
+			item.key.delete()
+		self.get()
+
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
 	('/index.html', MainHandler),
@@ -87,6 +86,7 @@ app = webapp2.WSGIApplication([
 	('/sold.html', SoldHandler),
 	('/add.html', AddHandler),
 	('/delete.html', DeleteHandler),
-	('/add', Add),
-	('/delete', Delete),
+	('/add', AddHandler),
+	('/delete', DeleteHandler),
+	('/buy', BuyHandler),
 ], debug=True)
